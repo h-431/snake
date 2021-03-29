0f284cacf8c763a23138ce25b439ac7ce32ca420
git <admin>
2021-03-29 22:41:17 +0300

    first commit

---
+++ src/MainWindow.java
@@ -0,0 +1,18 @@
+import sun.applet.Main;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+    public  MainWindow(){
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320,345);
+        setLocation(400,400);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+    }
+}
