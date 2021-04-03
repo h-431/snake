cc0c37952a5bf8e69e4c76753790011d75eb9081
git <admin>
2021-04-03 21:10:35 +0300

    3 выпуск Обработчики нажатия клавиш, отрисовка окна при поражении

--- src/GameField.java
+++ src/GameField.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public class GameField extends JPanel implements ActionListener {
@@ -28,6 +30,8 @@ public class GameField extends JPanel implements ActionListener {
         setBackground(Color.BLACK);
         loadImages();
         initGame();
+        addKeyListener(new FieldKeyListener());
+        setFocusable(true);
     }
 
     public void initGame(){
@@ -59,13 +63,19 @@ public void createApple()
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (inGame)
-        {
+        if (inGame) {
             g.drawImage(apple, appleX, appleY, this);
             for (int i = 0; i < dots; i++) {
                 g.drawImage(dot, x[i], y[i], this);
             }
         }
+            else
+            {
+             String str = "GAME OVER";
+             g.setColor(Color.WHITE);
+             g.drawString(str,125,SIZE/2);
+            }
+
     }
 
     public void move(){
@@ -129,8 +139,46 @@ public void createApple()
     public void actionPerformed(ActionEvent e) {
         if (inGame){
             checkApple();
+            checkCollisions();
             move();
         }
         repaint();
+    }
+
+
+    class FieldKeyListener extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            super.keyPressed(e);
+            int key = e.getKeyCode();
+            if (key == KeyEvent.VK_LEFT && !right)
+            {
+                left = true;
+                up = false;
+                down = false;
+            }
+
+            if (key == KeyEvent.VK_RIGHT && !left)
+            {
+                right = true;
+                up = false;
+                down = false;
+            }
+
+            if (key == KeyEvent.VK_UP && !down)
+            {
+                left = false;
+                up = true;
+                right = false;
+            }
+
+            if (key == KeyEvent.VK_DOWN && !up)
+            {
+                left = false;
+                right = false;
+                down = true;
+            }
+
+        }
     }
 }
